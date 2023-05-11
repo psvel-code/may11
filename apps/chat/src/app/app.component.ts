721b28e465deec89335b8178a744abe7189a50a4
psvel <pshunmugavelcentizen@gmail.com>
2023-05-11 10:30:47 +0530

    Initial commit

---
+++ apps/chat/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'may11-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'chat';
+}
